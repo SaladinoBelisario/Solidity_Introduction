66392f13f638e10e2ac9b9b2c65399013929e009
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-04-26 12:28:56 -0500

    Unit testing

---
+++ Dapps_examples/Ballot1/test/test_ballot.js
@@ -0,0 +1,78 @@
+let Ballot = artifacts.require("./Ballot.sol");
+let ballotInstance;
+
+contract ('Ballot Contract', function (accounts) {
+    // account[0] its the default account
+    // Test Case 1
+    it('Contract deployment', function () {
+        return Ballot.deployed().then(function (instance) {
+            ballotInstance = instance;
+            console.assert(ballotInstance != undefined, 'Ballot Contract should be defined');
+        });
+    });
+
+    // Test Case 2
+    it('Valid user registration', function () {
+        return ballotInstance.register(accounts[1], {from: accounts[0]}).then(function (result) {
+            console.assert.equal('0x01', result.receipt.status, 'Registration is valid');
+            return ballotInstance.register(accounts[2], {from: accounts[0]});
+        }).then(function (result) {
+            console.assert.equal('0x01', result.receipt.status, 'Registration is valid');
+            return ballotInstance.register(accounts[3], {from: accounts[0]});
+        }).then(function (result) {
+            console.assert.equal('0x01', result.receipt.status, 'Registration is valid');
+        });
+    });
+
+    // Test Case 3
+    it('Valid voting', function () {
+        return ballotInstance.vote(2, {from: accounts[0]}).then(function (result) {
+            console.assert.equal('0x01', result.receipt.status, 'Voting is done');
+            return ballotInstance.vote(1, {from: accounts[1]});
+        }).then(function (result) {
+            console.assert.equal('0x01', result.receipt.status, 'Voting is done');
+            return ballotInstance.register(1, {from: accounts[2]});
+        }).then(function (result) {
+            console.assert.equal('0x01', result.receipt.status, 'Voting is done');
+            return ballotInstance.register(1, {from: accounts[3]});
+        }).then(function (result) {
+            console.assert.equal('0x01', result.receipt.status, 'Voting is done');
+        });
+    });
+
+    // Test Case 4
+    it('Validate winner', function () {
+        return ballotInstance.winningProposal.call().then(function (result) {
+            console.assert.equal(1, result.toNumber(), 'Winner its validated with the expected winner');
+        });
+    });
+
+    // Negative tests
+    // Test Case 5
+    it('Should NOT accept unauthorized registrations', function () {
+        return ballotInstance.register(accounts[6], {from: accounts[1]})
+            .then(function (result) {
+                throw ("Condition not implemented in Smart Contract");
+            }).catch( function (e) {
+                if (e === "Condition not implemented in Smart Contract"){
+                    console.assert(false);
+                }else{
+                    console.assert(true);
+                }
+            })
+    });
+
+    // Test Case 6
+    it('Should NOT accept unregistered user vote', function () {
+        return ballotInstance.vote(1, {from: accounts[7]})
+            .then(function (result) {
+                throw ("Condition not implemented in Smart Contract");
+            }).catch( function (e) {
+                if (e === "Condition not implemented in Smart Contract"){
+                    console.assert(false);
+                }else{
+                    console.assert(true);
+                }
+            })
+    });
+});
