cbdc3c0bfdbca42c3c5206e69ace987d280b0743
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-04-27 11:36:56 -0500

    Ballot correction

--- Dapps_examples/Ballot1/contracts/Ballot.sol
+++ Dapps_examples/Ballot1/contracts/Ballot.sol
@@ -1,32 +1,37 @@
 pragma solidity ^0.4.22;
+
 contract Ballot {
 
     struct Voter {
         uint weight;
         bool voted;
         uint8 vote;
-       // address delegate;
+        // address delegate;
     }
-    struct Proposal {
+
+    //modifer
+    modifier onlyOwner () {
+        require(msg.sender == chairperson);
+        _;
+    }
+
+    /* struct Proposal {
         uint voteCount; // could add other data about proposal
-    }
+    } */
+    address public chairperson;
+    mapping(address => Voter) public voters;
+    uint[4] public proposals;
 
-    address chairperson;
-    mapping(address => Voter) voters;
-    Proposal[] proposals;
-    
-
-    /// Create a new ballot with $(_numProposals) different proposals.
-    constructor (uint8 _numProposals) public {
+    // Create a new ballot with 4 different proposals.
+    function Ballot() public {
         chairperson = msg.sender;
         voters[chairperson].weight = 2;
-        proposals.length = _numProposals; 
     }
 
     /// Give $(toVoter) the right to vote on this ballot.
     /// May only be called by $(chairperson).
-    function register(address toVoter) public {
-        if (msg.sender != chairperson || voters[toVoter].voted) return;
+    function register(address toVoter) public onlyOwner{
+        if(voters[toVoter].weight != 0) revert();
         voters[toVoter].weight = 1;
         voters[toVoter].voted = false;
     }
@@ -34,19 +39,19 @@ contract Ballot {
     /// Give a single vote to proposal $(toProposal).
     function vote(uint8 toProposal) public {
         Voter storage sender = voters[msg.sender];
-        if (sender.voted || toProposal >= proposals.length) return;
+        if (sender.voted || toProposal >= 4 || sender.weight == 0) revert();
         sender.voted = true;
         sender.vote = toProposal;
-        proposals[toProposal].voteCount += sender.weight;
+        proposals[toProposal] += sender.weight;
     }
 
     function winningProposal() public constant returns (uint8 _winningProposal) {
         uint256 winningVoteCount = 0;
-        for (uint8 prop = 0; prop < proposals.length; prop++)
-            if (proposals[prop].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[prop].voteCount;
+        for (uint8 prop = 0; prop < 4; prop++)
+            if (proposals[prop] > winningVoteCount) {
+                winningVoteCount = proposals[prop];
                 _winningProposal = prop;
             }
     }
 }
- 
+
--- Dapps_examples/Ballot1/test/test_ballot.js
+++ Dapps_examples/Ballot1/test/test_ballot.js
@@ -1,76 +1,83 @@
 let Ballot = artifacts.require("./Ballot.sol");
+
 let ballotInstance;
 
-contract ('Ballot Contract', function (accounts) {
-    // account[0] its the default account
-    // Test Case 1
-    it('Contract deployment', function () {
+contract('Ballot Contract', function (accounts) {
+    //accounts[0] is the default account
+    //Positive Test 1
+    it("Contract deployment", function() {
         return Ballot.deployed().then(function (instance) {
             ballotInstance = instance;
-            assert(ballotInstance != undefined, 'Ballot Contract should be defined');
+            assert(ballotInstance !== undefined, 'Ballot contract should be defined');
         });
     });
 
-    // Test Case 2
-    it('Valid user registration', function () {
-        return ballotInstance.register(accounts[1], {from: accounts[0]}).then(function (result) {
+    //Positive Test 2
+    it("Valid user registration", function() {
+        return ballotInstance.register(accounts[1], { from: accounts[0]}).then(function (result) {
             assert.equal('0x01', result.receipt.status, 'Registration is valid');
-            return ballotInstance.register(accounts[2], {from: accounts[0]});
+            return ballotInstance.register(accounts[2], { from: accounts[0]});
         }).then(function (result) {
             assert.equal('0x01', result.receipt.status, 'Registration is valid');
-            return ballotInstance.register(accounts[3], {from: accounts[0]});
-        }).then(function (result) {
+            return ballotInstance.register(accounts[3], { from: accounts[0]});
+        }).then(function(result) {
             assert.equal('0x01', result.receipt.status, 'Registration is valid');
         });
     });
 
-    // Test Case 3
-    it('Valid voting', function () {
+    //Positive Test 3
+    it("Valid voting", function() {
         return ballotInstance.vote(2, {from: accounts[0]}).then(function (result) {
             assert.equal('0x01', result.receipt.status, 'Voting is done');
             return ballotInstance.vote(1, {from: accounts[1]});
         }).then(function (result) {
             assert.equal('0x01', result.receipt.status, 'Voting is done');
-            return ballotInstance.register(1, {from: accounts[2]});
+            return ballotInstance.vote(1, {from: accounts[2]});
         }).then(function (result) {
             assert.equal('0x01', result.receipt.status, 'Voting is done');
-            return ballotInstance.register(1, {from: accounts[3]});
+            return ballotInstance.vote(1, {from: accounts[3]});
         }).then(function (result) {
             assert.equal('0x01', result.receipt.status, 'Voting is done');
         });
     });
 
-    // Test Case 4
-    it('Validate winner', function () {
+    //Positive Test 4
+    it("Validate winner", function () {
         return ballotInstance.winningProposal.call().then(function (result) {
-            assert.equal(1, result.toNumber(), 'Winner its validated with the expected winner');
+            assert.equal(1, result.toNumber(), 'Winner is validated with the expected winner');
         });
     });
 
-    // Negative tests
-    // Test Case 5
-    it('Should NOT accept unauthorized registrations', function () {
-        return ballotInstance.register(accounts[6], {from: accounts[1]})
+    //Negative Test 5
+    it("Should NOT accept unauthorized registration", function () {
+        return ballotInstance.register(accounts[6], { from: accounts[1]})
             .then(function (result) {
-                throw ("Condition not implemented in Smart Contract");
-            }).catch( function (e) {
-                if (e === "Condition not implemented in Smart Contract"){
+                /* 	If the user had handled the above mentioned failure case in solidity,
+                    then this block will not be executed.
+                    Truffle would directly throw the revert error which will be catched
+                */
+                throw("Condition not implemented in Smart Contract");
+            }).catch(function (e) {
+                /*	If the error is custom thrown then the condition was not checked and hence fail the test case
+                    else pass the test case
+                */
+                if(e === "Condition not implemented in Smart Contract") {
                     assert(false);
-                }else{
+                } else {
                     assert(true);
                 }
             })
     });
 
-    // Test Case 6
-    it('Should NOT accept unregistered user vote', function () {
+    //Negative Test 6
+    it("Should NOT accept unregistered user vote", function () {
         return ballotInstance.vote(1, {from: accounts[7]})
             .then(function (result) {
-                throw ("Condition not implemented in Smart Contract");
-            }).catch( function (e) {
-                if (e === "Condition not implemented in Smart Contract"){
+                throw("Condition not implemented in Smart Contract");
+            }).catch(function (e) {
+                if(e === "Condition not implemented in Smart Contract") {
                     assert(false);
-                }else{
+                } else {
                     assert(true);
                 }
             })
